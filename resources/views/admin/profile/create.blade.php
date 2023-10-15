209a8201433015d6de15e615c05a60eb2974856a
tetsuya <tetsuya0817com@gmail.com>
2023-10-15 11:49:56 +0000

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+         {{-- 後の章で説明します --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        {{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}
+        <title>@section('title', 'プロフィール')</title>
+
+        <!-- Scripts -->
+         {{-- Laravel標準で用意されているJavascriptを読み込みます --}}
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        {{-- Laravel標準で用意されているCSSを読み込みます --}}
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        {{-- この章の後半で作成するCSSを読み込みます --}}
+         <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            {{-- 画面上部に表示するナビゲーションバーです。 --}}
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav">
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {{-- ここまでナビゲーションバー --}}
+
+            <main class="py-4">
+                {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
+                @section('content')
+                    <div class="container">
+                         <div class="row">
+                            <div class="col-md-8 mx-auto">
+                                <h2>プロフィール</h2>
+            　　　　　　　　　　　　</div>
+      　　　　　　　　　　　　　　　  </div>
+   　　　　　　　　　　　　　　　 </div>
+　　　　　　　　@endsection
+            </main>
+        </div>
+    </body>
+</html>
