9c578be41e2bdb39c4b9e52b6c73f5dc3b281cb0
tetsuya <tetsuya0817com@gmail.com>
2023-10-15 02:50:38 +0000

    first commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -13,22 +13,3 @@ class NewsController extends Controller
         return view('admin.news.create');
     }
 }
-　　public function add()
-    {
-        return view('admin.profile.create');
-    }
-
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
-
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
---
+++ app/Http/Controllers/Admin/news/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+    </body>
+</html>
