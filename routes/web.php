51d8ab4f0feb4d50bc55ff2734a20de88318b211
tetsuya <tetsuya0817com@gmail.com>
2023-11-13 02:22:09 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -19,22 +19,22 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
     Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create', 'create')->name('news.create');
     Route::get('news', 'index')->name('news.index');
+    Route::get('news/edit', 'edit')->name('news.edit');
+    Route::post('news/edit', 'update')->name('news.update');
+    Route::get('news/delete', 'delete')->name('news.delete');
 });
-
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->middleware('auth')->group(function() {
-    Route::get('profile/create', 'add')->name('profile.add');
     Route::post('profile/create', 'create')->name('profile.create'); 
 });
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/edit', 'edit')->name('profile.add');
-    Route::post('profile/update', 'update')->name('profile.update');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::post('profile/edit', 'edit')->name('profile.edit');
 });
 Auth::routes();
 
 use App\Http\Controllers\NewsController as PublicNewsController;
-Route::get('/', [PublicNewsController::class, 'index'])->name('profile.index');
+Route::get('/', [PublicNewsController::class, 'index'])->name('news.index');
 
 use App\Http\Controllers\ProfileController as PublicProfileController;
 Route::get('/', [PublicProfileController::class, 'index'])->name('profile.index');
